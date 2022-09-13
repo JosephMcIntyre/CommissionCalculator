3e2bdb7de2ab56ca4cb9d68376e932aa49c2871b
“JosephMcIntyre” <Joseph_McIntyre@icloud.com>
2022-09-13 16:30:34 -0400

    Initial Commit

---
+++ RealmTest/RealmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTestApp.swift
+//  RealmTest
+//
+//  Created by Joseph McIntyre on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
